901b953f675923c9e27510448f604b74608336f3
Blandus1 <blandine0045@gmail.com>
2025-07-04 20:10:39 -0700

    second commit

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ const PORT= process.env.PORT;
 
 const server= http.createServer((req,res)=>{
 
+if(req.url === '/'){
+
     res.writeHead(200,{'Content-type':'text/html'});
-    res.end('<h1>Hello world! Now it is working</h1><P> I am so SO happy!!!!');
+    res.end('<h1>Homepage</h1>');
+}else if( req.url=== '/about'){
+     res.writeHead(200,{'Content-type':'text/html'});
+    res.end('<h1>About</h1>');
+}else{
+    res.writeHead(404,{'Content-type':'text/html'});
+    res.end('<h1>Not found</h1>'); 
+}
+
+    
+   
 }).listen(PORT,()=>{
     console.log(`server running on port ${PORT}`)
 })
